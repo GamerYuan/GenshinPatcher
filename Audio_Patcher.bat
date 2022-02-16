9fe8b5c4bc8f0480bc6822f4db89417ad35507af
GamerYuan <99809097+GamerYuan@users.noreply.github.com>
2022-02-16 21:07:26 +0800

    Add files via upload
    The main batch file. Read README for more information

---
+++ Audio_Patcher.bat
@@ -0,0 +1,366 @@
+@echo off
+
+hpatchz -f Banks0.pck Banks0.pck.hdiff Banks0.pck
+hpatchz -f Banks1.pck Banks1.pck.hdiff Banks1.pck
+hpatchz -f Banks2.pck Banks2.pck.hdiff Banks2.pck
+hpatchz -f Banks3.pck Banks3.pck.hdiff Banks3.pck
+hpatchz -f Banks4.pck Banks4.pck.hdiff Banks4.pck
+hpatchz -f Banks5.pck Banks5.pck.hdiff Banks5.pck
+hpatchz -f Banks6.pck Banks6.pck.hdiff Banks6.pck
+hpatchz -f Banks7.pck Banks7.pck.hdiff Banks7.pck
+hpatchz -f Banks8.pck Banks8.pck.hdiff Banks8.pck
+hpatchz -f Banks9.pck Banks9.pck.hdiff Banks9.pck
+hpatchz -f Banks10.pck Banks10.pck.hdiff Banks10.pck
+hpatchz -f Banks11.pck Banks11.pck.hdiff Banks11.pck
+hpatchz -f Banks12.pck Banks12.pck.hdiff Banks12.pck
+hpatchz -f Banks13.pck Banks13.pck.hdiff Banks13.pck
+hpatchz -f Banks14.pck Banks14.pck.hdiff Banks14.pck
+hpatchz -f Banks15.pck Banks15.pck.hdiff Banks15.pck
+hpatchz -f Banks16.pck Banks16.pck.hdiff Banks16.pck
+hpatchz -f Banks17.pck Banks17.pck.hdiff Banks17.pck
+hpatchz -f Banks18.pck Banks18.pck.hdiff Banks18.pck
+hpatchz -f Banks19.pck Banks19.pck.hdiff Banks19.pck
+hpatchz -f Banks20.pck Banks20.pck.hdiff Banks20.pck
+hpatchz -f Banks21.pck Banks21.pck.hdiff Banks21.pck
+hpatchz -f Banks22.pck Banks22.pck.hdiff Banks22.pck
+hpatchz -f Banks23.pck Banks23.pck.hdiff Banks23.pck
+hpatchz -f Banks24.pck Banks24.pck.hdiff Banks24.pck
+hpatchz -f Banks25.pck Banks25.pck.hdiff Banks25.pck
+hpatchz -f Banks26.pck Banks26.pck.hdiff Banks26.pck
+hpatchz -f Banks27.pck Banks27.pck.hdiff Banks27.pck
+hpatchz -f Banks28.pck Banks28.pck.hdiff Banks28.pck
+hpatchz -f Banks29.pck Banks29.pck.hdiff Banks29.pck
+hpatchz -f Banks30.pck Banks30.pck.hdiff Banks30.pck
+hpatchz -f Banks31.pck Banks31.pck.hdiff Banks31.pck
+hpatchz -f Minimum.pck Minimum.pck.hdiff Minimum.pck
+hpatchz -f Music0.pck Music0.pck.hdiff Music0.pck
+hpatchz -f Music1.pck Music1.pck.hdiff Music1.pck
+hpatchz -f Music2.pck Music2.pck.hdiff Music2.pck
+hpatchz -f Music3.pck Music3.pck.hdiff Music3.pck
+hpatchz -f Music4.pck Music4.pck.hdiff Music4.pck
+hpatchz -f Music5.pck Music5.pck.hdiff Music5.pck
+hpatchz -f Music6.pck Music6.pck.hdiff Music6.pck
+hpatchz -f Music7.pck Music7.pck.hdiff Music7.pck
+hpatchz -f Music8.pck Music8.pck.hdiff Music8.pck
+hpatchz -f Music9.pck Music9.pck.hdiff Music9.pck
+hpatchz -f Music10.pck Music10.pck.hdiff Music10.pck
+hpatchz -f Music11.pck Music11.pck.hdiff Music11.pck
+hpatchz -f Music12.pck Music12.pck.hdiff Music12.pck
+hpatchz -f Music13.pck Music13.pck.hdiff Music13.pck
+hpatchz -f Music14.pck Music14.pck.hdiff Music14.pck
+hpatchz -f Music15.pck Music15.pck.hdiff Music15.pck
+hpatchz -f Music16.pck Music16.pck.hdiff Music16.pck
+hpatchz -f Music17.pck Music17.pck.hdiff Music17.pck
+hpatchz -f Music18.pck Music18.pck.hdiff Music18.pck
+hpatchz -f Music19.pck Music19.pck.hdiff Music19.pck
+hpatchz -f Music20.pck Music20.pck.hdiff Music20.pck
+hpatchz -f Music21.pck Music21.pck.hdiff Music21.pck
+hpatchz -f Music22.pck Music22.pck.hdiff Music22.pck
+hpatchz -f Music23.pck Music23.pck.hdiff Music23.pck
+hpatchz -f Music24.pck Music24.pck.hdiff Music24.pck
+hpatchz -f Music25.pck Music25.pck.hdiff Music25.pck
+hpatchz -f Music26.pck Music26.pck.hdiff Music26.pck
+hpatchz -f Music27.pck Music27.pck.hdiff Music27.pck
+hpatchz -f Music28.pck Music28.pck.hdiff Music28.pck
+hpatchz -f Music29.pck Music29.pck.hdiff Music29.pck
+hpatchz -f Music30.pck Music30.pck.hdiff Music30.pck
+hpatchz -f Music31.pck Music31.pck.hdiff Music31.pck
+hpatchz -f Streamed0.pck Streamed0.pck.hdiff Streamed0.pck
+hpatchz -f Streamed1.pck Streamed1.pck.hdiff Streamed1.pck
+hpatchz -f Streamed2.pck Streamed2.pck.hdiff Streamed2.pck
+hpatchz -f Streamed3.pck Streamed3.pck.hdiff Streamed3.pck
+hpatchz -f Streamed4.pck Streamed4.pck.hdiff Streamed4.pck
+hpatchz -f Streamed5.pck Streamed5.pck.hdiff Streamed5.pck
+hpatchz -f Streamed6.pck Streamed6.pck.hdiff Streamed6.pck
+hpatchz -f Streamed7.pck Streamed7.pck.hdiff Streamed7.pck
+hpatchz -f Streamed8.pck Streamed8.pck.hdiff Streamed8.pck
+hpatchz -f Streamed9.pck Streamed9.pck.hdiff Streamed9.pck
+hpatchz -f Streamed10.pck Streamed10.pck.hdiff Streamed10.pck
+hpatchz -f Streamed11.pck Streamed11.pck.hdiff Streamed11.pck
+hpatchz -f Streamed12.pck Streamed12.pck.hdiff Streamed12.pck
+hpatchz -f Streamed13.pck Streamed13.pck.hdiff Streamed13.pck
+hpatchz -f Streamed14.pck Streamed14.pck.hdiff Streamed14.pck
+hpatchz -f Streamed15.pck Streamed15.pck.hdiff Streamed15.pck
+hpatchz -f Streamed16.pck Streamed16.pck.hdiff Streamed16.pck
+hpatchz -f Streamed17.pck Streamed17.pck.hdiff Streamed17.pck
+hpatchz -f Streamed18.pck Streamed18.pck.hdiff Streamed18.pck
+hpatchz -f Streamed19.pck Streamed19.pck.hdiff Streamed19.pck
+hpatchz -f Streamed20.pck Streamed20.pck.hdiff Streamed20.pck
+hpatchz -f Streamed21.pck Streamed21.pck.hdiff Streamed21.pck
+hpatchz -f Streamed22.pck Streamed22.pck.hdiff Streamed22.pck
+hpatchz -f Streamed23.pck Streamed23.pck.hdiff Streamed23.pck
+hpatchz -f Streamed24.pck Streamed24.pck.hdiff Streamed24.pck
+hpatchz -f Streamed25.pck Streamed25.pck.hdiff Streamed25.pck
+hpatchz -f Streamed26.pck Streamed26.pck.hdiff Streamed26.pck
+hpatchz -f Streamed27.pck Streamed27.pck.hdiff Streamed27.pck
+hpatchz -f Streamed28.pck Streamed28.pck.hdiff Streamed28.pck
+hpatchz -f Streamed29.pck Streamed29.pck.hdiff Streamed29.pck
+hpatchz -f Streamed30.pck Streamed30.pck.hdiff Streamed30.pck
+hpatchz -f Streamed31.pck Streamed31.pck.hdiff Streamed31.pck
+
+del Banks0.pck.hdiff
+del Banks1.pck.hdiff
+del Banks2.pck.hdiff
+del Banks3.pck.hdiff
+del Banks4.pck.hdiff
+del Banks5.pck.hdiff
+del Banks6.pck.hdiff
+del Banks7.pck.hdiff
+del Banks8.pck.hdiff
+del Banks9.pck.hdiff
+del Banks10.pck.hdiff
+del Banks11.pck.hdiff
+del Banks12.pck.hdiff
+del Banks13.pck.hdiff
+del Banks14.pck.hdiff
+del Banks15.pck.hdiff
+del Banks16.pck.hdiff
+del Banks17.pck.hdiff
+del Banks18.pck.hdiff
+del Banks19.pck.hdiff
+del Banks20.pck.hdiff
+del Banks21.pck.hdiff
+del Banks22.pck.hdiff
+del Banks23.pck.hdiff
+del Banks24.pck.hdiff
+del Banks25.pck.hdiff
+del Banks26.pck.hdiff
+del Banks27.pck.hdiff
+del Banks28.pck.hdiff
+del Banks29.pck.hdiff
+del Banks30.pck.hdiff
+del Banks31.pck.hdiff
+del Minimum.pck.hdiff
+del Music0.pck.hdiff
+del Music1.pck.hdiff
+del Music2.pck.hdiff
+del Music3.pck.hdiff
+del Music4.pck.hdiff
+del Music5.pck.hdiff
+del Music6.pck.hdiff
+del Music7.pck.hdiff
+del Music8.pck.hdiff
+del Music9.pck.hdiff
+del Music10.pck.hdiff
+del Music11.pck.hdiff
+del Music12.pck.hdiff
+del Music13.pck.hdiff
+del Music14.pck.hdiff
+del Music15.pck.hdiff
+del Music16.pck.hdiff
+del Music17.pck.hdiff
+del Music18.pck.hdiff
+del Music19.pck.hdiff
+del Music20.pck.hdiff
+del Music21.pck.hdiff
+del Music22.pck.hdiff
+del Music23.pck.hdiff
+del Music24.pck.hdiff
+del Music25.pck.hdiff
+del Music26.pck.hdiff
+del Music27.pck.hdiff
+del Music28.pck.hdiff
+del Music29.pck.hdiff
+del Music30.pck.hdiff
+del Music31.pck.hdiff
+del Streamed0.pck.hdiff
+del Streamed1.pck.hdiff
+del Streamed2.pck.hdiff
+del Streamed3.pck.hdiff
+del Streamed4.pck.hdiff
+del Streamed5.pck.hdiff
+del Streamed6.pck.hdiff
+del Streamed7.pck.hdiff
+del Streamed8.pck.hdiff
+del Streamed9.pck.hdiff
+del Streamed10.pck.hdiff
+del Streamed11.pck.hdiff
+del Streamed12.pck.hdiff
+del Streamed13.pck.hdiff
+del Streamed14.pck.hdiff
+del Streamed15.pck.hdiff
+del Streamed16.pck.hdiff
+del Streamed17.pck.hdiff
+del Streamed18.pck.hdiff
+del Streamed19.pck.hdiff
+del Streamed20.pck.hdiff
+del Streamed21.pck.hdiff
+del Streamed22.pck.hdiff
+del Streamed23.pck.hdiff
+del Streamed24.pck.hdiff
+del Streamed25.pck.hdiff
+del Streamed26.pck.hdiff
+del Streamed27.pck.hdiff
+del Streamed28.pck.hdiff
+del Streamed29.pck.hdiff
+del Streamed30.pck.hdiff
+del Streamed31.pck.hdiff
+
+Del Music_1.2_0.pck
+Del Music_1.2_1.pck
+Del Music_1.2_2.pck
+Del Music_1.2_3.pck
+Del Music_1.3_0.pck
+Del Music_1.4_0.pck
+Del Music_1.4_1.pck
+Del Music_1.4_2.pck
+Del Music_1.4_3.pck
+Del Music_1.5_0.pck
+Del Music_1.5_1.pck
+Del Music_1.5_2.pck
+Del Music_1.5_3.pck
+Del Music_1.5_4.pck
+Del Music_1.5_5.pck
+Del Music_1.6_0.pck
+Del Music_1.6_1.pck
+Del Music_1.6_2.pck
+Del Music_1.6_3.pck
+Del Music_2.0_0.pck
+Del Music_2.0_1.pck
+Del Music_2.0_10.pck
+Del Music_2.0_11.pck
+Del Music_2.0_12.pck
+Del Music_2.0_13.pck
+Del Music_2.0_2.pck
+Del Music_2.0_3.pck
+Del Music_2.0_4.pck
+Del Music_2.0_5.pck
+Del Music_2.0_6.pck
+Del Music_2.0_7.pck
+Del Music_2.0_8.pck
+Del Music_2.0_9.pck
+Del Music_2.1_0.pck
+Del Music_2.1_1.pck
+Del Music_2.1_2.pck
+Del Music_2.1_3.pck
+Del Music_2.1_4.pck
+Del Music_2.1_5.pck
+Del Music_2.1_6.pck
+Del Music_2.1_7.pck
+Del Music_2.1_8.pck
+Del Music_2.2_0.pck
+Del Music_2.2_1.pck
+Del Music_2.2_2.pck
+Del Music_2.2_3.pck
+Del Music_2.2_4.pck
+Del Music_2.2_5.pck
+Del Music_2.3_0.pck
+Del Music_2.4_0.pck
+Del Music_2.4_1.pck
+Del Music_2.4_2.pck
+Del Music_2.4_3.pck
+Del Music_2.4_4.pck
+Del Music_2.4_5.pck
+Del Music_2.4_6.pck
+Del Music_2.4_7.pck
+Del Music_2.4_8.pck
+Del Music_2.4_9.pck
+Del SFX_1.2_0.pck
+Del SFX_1.2_1.pck
+Del SFX_1.2_2.pck
+Del SFX_1.2_3.pck
+Del SFX_1.2_4.pck
+Del SFX_1.3_0.pck
+Del SFX_1.3_1.pck
+Del SFX_1.4_0.pck
+Del SFX_1.4_1.pck
+Del SFX_1.4_2.pck
+Del SFX_1.4_3.pck
+Del SFX_1.4_4.pck
+Del SFX_1.5_0.pck
+Del SFX_1.5_1.pck
+Del SFX_1.5_2.pck
+Del SFX_1.5_3.pck
+Del SFX_1.5_4.pck
+Del SFX_1.5_5.pck
+Del SFX_1.5_6.pck
+Del SFX_1.5_7.pck
+Del SFX_1.6_0.pck
+Del SFX_1.6_1.pck
+Del SFX_1.6_10.pck
+Del SFX_1.6_11.pck
+Del SFX_1.6_12.pck
+Del SFX_1.6_13.pck
+Del SFX_1.6_14.pck
+Del SFX_1.6_15.pck
+Del SFX_1.6_16.pck
+Del SFX_1.6_17.pck
+Del SFX_1.6_18.pck
+Del SFX_1.6_19.pck
+Del SFX_1.6_2.pck
+Del SFX_1.6_20.pck
+Del SFX_1.6_21.pck
+Del SFX_1.6_22.pck
+Del SFX_1.6_23.pck
+Del SFX_1.6_24.pck
+Del SFX_1.6_25.pck
+Del SFX_1.6_26.pck
+Del SFX_1.6_27.pck
+Del SFX_1.6_28.pck
+Del SFX_1.6_29.pck
+Del SFX_1.6_3.pck
+Del SFX_1.6_4.pck
+Del SFX_1.6_5.pck
+Del SFX_1.6_6.pck
+Del SFX_1.6_7.pck
+Del SFX_1.6_8.pck
+Del SFX_1.6_9.pck
+Del SFX_2.0_0.pck
+Del SFX_2.0_1.pck
+Del SFX_2.0_2.pck
+Del SFX_2.0_3.pck
+Del SFX_2.0_4.pck
+Del SFX_2.0_5.pck
+Del SFX_2.0_6.pck
+Del SFX_2.0_7.pck
+Del SFX_2.1_0.pck
+Del SFX_2.1_1.pck
+Del SFX_2.1_10.pck
+Del SFX_2.1_11.pck
+Del SFX_2.1_12.pck
+Del SFX_2.1_13.pck
+Del SFX_2.1_14.pck
+Del SFX_2.1_15.pck
+Del SFX_2.1_16.pck
+Del SFX_2.1_2.pck
+Del SFX_2.1_3.pck
+Del SFX_2.1_4.pck
+Del SFX_2.1_5.pck
+Del SFX_2.1_6.pck
+Del SFX_2.1_7.pck
+Del SFX_2.1_8.pck
+Del SFX_2.1_9.pck
+Del SFX_2.2_0.pck
+Del SFX_2.2_1.pck
+Del SFX_2.3_0.pck
+Del SFX_2.3_1.pck
+Del SFX_2.3_2.pck
+Del SFX_2.3_a_0.pck
+Del SFX_2.4_0.pck
+Del SFX_2.4_1.pck
+Del SFX_2.4_10.pck
+Del SFX_2.4_11.pck
+Del SFX_2.4_12.pck
+Del SFX_2.4_13.pck
+Del SFX_2.4_14.pck
+Del SFX_2.4_15.pck
+Del SFX_2.4_16.pck
+Del SFX_2.4_17.pck
+Del SFX_2.4_18.pck
+Del SFX_2.4_19.pck
+Del SFX_2.4_2.pck
+Del SFX_2.4_20.pck
+Del SFX_2.4_21.pck
+Del SFX_2.4_3.pck
+Del SFX_2.4_4.pck
+Del SFX_2.4_5.pck
+Del SFX_2.4_6.pck
+Del SFX_2.4_7.pck
+Del SFX_2.4_8.pck
+Del SFX_2.4_9.pck
+
+del hpatchz.exe
+
+del Audio_Patcher.bat
+
+pause
